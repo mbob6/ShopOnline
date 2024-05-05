588e4c7ad95e792840fb6cb7a4bb9e4db6b58d8d
Oni Bashir Muh'Bashir <108095967+mbob6@users.noreply.github.com>
2024-05-05 17:05:52 +0100

    Initialized Entity Framework

---
+++ ShopOnline.Api/Data/ShopOnlineDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShopOnline.Api.Data;
+
+public class ShopOnlineDbContext : DbContext
+{
+    public ShopOnlineDbContext(DbContextOptions<ShopOnlineDbContext> options) : base(options)   
+    {
+        
+    }
+}
--- ShopOnline.Api/obj/Debug/net8.0/ShopOnline.Api.AssemblyInfo.cs
+++ ShopOnline.Api/obj/Debug/net8.0/ShopOnline.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShopOnline.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+008b342f364e002ad7593d89e4355852818fe1ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7449a73b085df519cb6046af47aa1a4d21a19ae8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShopOnline.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShopOnline.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
